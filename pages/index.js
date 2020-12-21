249e41e3063e9d4fa4d03b23f2d8c5cb24d5252e
reagan-smith <reaganm.smith@yahoo.com>
2020-12-21 17:36:34 -0500

    file adjustment

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,196 @@
-import Head from "next/head";
+import * as React from "react";
+import { listOfFilters } from "./listOfFilters";
+import Select from "react-select";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import { Navbar } from "react-bootstrap";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Switch from "@material-ui/core/Switch";
+import FormGroup from "@material-ui/core/FormGroup";
+import { includes } from "lodash";
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    width: 300,
+  },
+  chips: {
+    display: "flex",
+    flexWrap: "wrap",
+  },
+  chip: {
+    margin: 2,
+  },
+  noLabel: {
+    marginTop: theme.spacing(3),
+  },
+}));
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+export default function FilterPage() {
+  const classes = useStyles();
+  const [selectedObjects, setSelectedObjects] = React.useState({});
+  const [displayedFilters, setDisplayedFilters] = React.useState(
+    listOfFilters.filter((filter) => filter.showByDefault)
+  );
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+  const handleChange = (event, columnHeader) => {
+    setSelectedObjects({
+      ...selectedObjects,
+      [columnHeader]: event,
+    });
+  };
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+  const handleToggle = (columnHeader) => {
+    setSelectedObjects({
+      ...selectedObjects,
+      [columnHeader]: !selectedObjects[columnHeader],
+    });
+  };
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+  const enumSelect = (filter) => {
+    const optionsObj = filter.possibleValues.map((val) => {
+      return { value: val, label: val };
+    });
+    return (
+      <div className="filter-grouping">
+        <div className="filter-header">{filter.columnDisplayName}</div>
+        <Select
+          isMulti
+          value={selectedObjects[filter.columnHeader]}
+          onChange={(event) => handleChange(event, filter.columnHeader)}
+          options={optionsObj}
+        />
+      </div>
+    );
+  };
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+  const dateSelect = (filter) => (
+    <div className="filter-grouping">
+      <div className="filter-header">{filter.columnDisplayName}</div>
+      <TextField
+        id={filter.columnHeader}
+        style={{ width: "100%" }}
+        type="datetime-local"
+        defaultValue="2017-05-24T10:30"
+        className={classes.textField}
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+    </div>
+  );
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+  const booleanSelect = (filter) => (
+    <FormGroup>
+      <FormControlLabel
+        control={
+          <Switch
+            checked={selectedObjects[filter.columnHeader]}
+            onChange={() => handleToggle(filter.columnHeader)}
+          />
+        }
+        label="Show only Prod Data"
+        style={{
+          paddingLeft: "1rem",
+          color: "hsl(0,0%,50%)",
+          marginBottom: 0,
+          marginTop: "0.5rem",
+        }}
+      />
+    </FormGroup>
+  );
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+  const inputType = (filter) => {
+    switch (filter.columnType) {
+      case "enum":
+        return enumSelect(filter);
+      case "string":
+        return enumSelect(filter);
+      case "dateRange":
+        return dateSelect(filter);
+      case "boolean":
+        return booleanSelect(filter);
+      default:
+        return enumSelect(filter);
+    }
+  };
+
+  const addNewFilterCallback = (event) => {
+    const filterToAdd = listOfFilters.filter((filter) =>
+      event.value.includes(filter.columnHeader)
+    );
+    setDisplayedFilters(displayedFilters.concat(filterToAdd));
+    if (typeof event.value !== "string") {
+      setSelectedObjects({
+        ...selectedObjects,
+        [event.value[0]]: [{ label: event.value[1], value: event.value[1] }],
+      });
+    }
+  };
+
+  const addNewFilters = () => {
+    const filtersToAdd = listOfFilters.filter(
+      (filter) => !includes(displayedFilters, filter)
+    );
+    const options = filtersToAdd
+      .map((filter) => {
+        if (filter.columnType == "enum") {
+          return filter.possibleValues.map((val) => {
+            return {
+              value: [filter.columnHeader, val],
+              label: `${filter.columnDisplayName}: ${val}`,
+            };
+          });
+        }
+        return {
+          value: filter.columnHeader,
+          label: filter.columnDisplayName,
+        };
+      })
+      .flat();
+    return (
+      <div className="filter-grouping">
+        <div
+          style={{
+            fontSize: "16px",
+          }}
         >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
+          Add New Filter
+        </div>
+        <Select value={[]} onChange={addNewFilterCallback} options={options} />
+      </div>
+    );
+  };
 
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src="/galaxy-logo.jpg"
+            width="348"
+            height="68"
+            style={{ paddingLeft: "1rem" }}
+          />{" "}
+        </Navbar.Brand>
+      </Navbar>
+      <div style={{ display: "flex" }}>
+        <div
+          className="col-3"
+          style={{
+            display: "grid",
+            borderRight: "solid 1px #ddd",
+            paddingTop: "1rem",
+            height: "fit-content",
+          }}
+        >
+          {displayedFilters.map((filter) => inputType(filter))}
+          <div style={{ borderBottom: "solid 1px #ddd", height: "1.5rem" }} />
+          {addNewFilters()}
+        </div>
+        <div className="col-9">{JSON.stringify(selectedObjects)}</div>
+      </div>
     </div>
   );
 }
