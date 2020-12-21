9116833118eb6f1d9d2b2877d9fed37e60e16e69
reagan-smith <reaganm.smith@yahoo.com>
2020-12-21 18:49:01 -0500

    created app file that exports actual app

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
 import App from "../src/pages/_app";
-
+import "../../styles/main.scss";
+import "fontsource-roboto";
 export default App;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,3 @@
-import "../../styles/main.scss";
-import "fontsource-roboto";
-
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
